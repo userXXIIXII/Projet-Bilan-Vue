037d18054cd324fb1f3dc1a595ad0a37358168b2
Aarin Deb <aarindeb2003@gmail.com>
2026-05-09 14:03:22 +0200

    Modal et project.vue

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -1,3 +1,161 @@
 <template>
-    
+    <transition name="modal">
+        <div v-if="isOpen" class="modal" @click.self="$emit('close')">
+
+            <div class="modal-inner">
+                <button @click="$emit('close')">X</button>
+
+                <h2>{{ titre }}</h2>
+                <img :src="image" class="modal-image" alt="Image de la modal">
+                <p>{{ description }}</p>
+
+                <div class="buttons">
+                    <a :href="demo" target="_blank" class="btn demo">Démo</a>
+                    <a :href="github" target="_blank" class="btn github">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </transition>
 </template>
+
+<script setup>
+    defineProps([
+        "isOpen",
+        "titre",
+        "description",
+        "image",
+        "demo",
+        "github"
+    ])
+
+    const emit = defineEmits(["close"])
+</script>
+
+<style scoped>
+    /* Animation */
+    .modal-enter-active,
+    .modal-leave-active {
+        transition: opacity 0.3s ease;
+    }
+
+    .modal-enter-from,
+    .modal-leave-to {
+        opacity: 0;
+    }
+
+    .modal-inner {
+        transform: scale(1);
+        transition: all 0.3s ease;
+    }
+
+    /*état de départ */
+    .modal-enter-from .modal-inner {
+        transform: scale(0.8);
+        opacity: 0;
+    }
+
+    /*état de sortie */
+    .modal-leave-to .modal-inner {
+        transform: scale(0.8);
+        opacity: 0;
+    }
+
+    .modal {
+        position: fixed;
+        inset: 0;
+
+        background: rgba(0, 0, 0, 0.6);
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        padding: 20px;
+        z-index: 2000;
+    }
+
+    .modal-inner {
+        position: relative; 
+
+        background: #1e1e1e;
+        color: white;
+
+        padding: 20px;
+        border-radius: 10px;
+
+        max-height: 85vh;
+        overflow-y: auto;
+
+        width: 300px;
+        max-width: 90%;
+
+        text-align: center;
+    }
+
+    .modal-image {
+        width: 100%;
+        object-fit: cover;
+        border-radius: 8px;
+        margin: 10px 0;
+        box-shadow: 10px 10px 20px black;
+    }
+
+    .modal-inner button {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+
+        background: none;
+        border: none;
+        color: white;
+        font-size: 18px;
+        cursor: pointer;
+    } 
+
+    p {
+        font-size: medium;
+    }
+
+    .buttons {
+        display: flex;
+        justify-content: center;
+        gap: 15px;
+        margin-top: 20px;
+    }
+
+    .btn {
+        padding: 10px 15px;
+        border-radius: 8px;
+        text-decoration: none;
+        font-size: 14px;
+        cursor: pointer;
+        transition: 0.3s;
+    }
+
+    /* bouton démo */
+    .btn.demo {
+        background: #7f5af0;
+        color: white;
+    }
+
+    /* bouton github */
+    .btn.github {
+        border: 2px solid #7f5af0;
+        color: #7f5af0;
+        background: transparent;
+    }
+
+    /* hover */
+        .btn:hover {
+        transform: translateY(-2px);
+    }
+
+    .btn.demo:hover {
+        background: #6c4de0;
+    }
+
+    .btn.github:hover {
+        background: #7f5af0;
+        color: white;
+    }
+</style>
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,5 +1,147 @@
 <template>
     <section>
-        <p>Mes projets</p>
+        <h1>Mes réalisations</h1>
+
+        <div class="projects">
+            <div v-for="p in projects" :key="p.id" class="card">
+                <h3>{{ p.titre }}</h3>
+                <img :src="p.preview" class="card-image" alt="Image du Projet">
+
+                <button @click="selected = p">Voir</button>
+            </div>
+        </div>
+
+        <Modal 
+            :isOpen="selected !== null"
+            :titre="selected?.titre"
+            :description="selected?.description"
+            :image="selected?.image"
+            :demo="selected?.demo"
+            :github="selected?.github"
+            @close="selected = null"
+
+        />
+
+        <p class="bio">
+            Sur cette page, vous trouverez un aperçu détaillé de 
+            mes réalisations en développement web, du front-end 
+            au back-end. Chaque projet illustre non seulement ma 
+            maîtrise des technologies modernes, mais aussi ma 
+            capacité à concevoir des interfaces intuitives, à résoudre 
+            des problèmes complexes et à créer des expériences 
+            utilisateurs fluides et engageantes.
+        </p>
     </section>
 </template>
+
+<script setup>
+    import Modal from './Modal/Modal.vue'
+    import { ref } from "vue"
+
+    import meteoImg from '@/assets/weather-app-img.png'
+    import meteoIcon from '@/assets/weather-app.png'
+    import cvImg from '@/assets/cvImg.png'
+    import cvIcon from '@/assets/cvIcon.png'
+
+    //Modal fermée par défaut
+
+    const selected = ref(null)
+
+    const projects = [
+        {
+            id: 1,
+            titre: "App météo - 2026",
+            description: "Ce projet est une application météo développée en HTML, CSS et JavaScript, permettant d’afficher la météo grâce à JavaScript et à l’utilisation d’une API.",
+            image: meteoImg,
+            preview: meteoIcon,
+            demo: "https://userxxiixii.github.io/weather_app/",
+            github: "https://github.com/userXXIIXII/weather_app"
+        },
+
+        {
+            id: 2,
+            titre: "CV HTML - 2024",
+            description: "CV développé en HTML, mettant en avant une structure claire et sémantique pour présenter mes informations personnelles, mes compétences et mon parcours.",
+            image: cvImg,
+            preview: cvIcon,
+            demo: "https://userxxiixii.github.io/CV-CEF/",
+            github: "https://github.com/userXXIIXII/CV-CEF"
+        }
+    ]
+</script>
+
+<style scoped>
+    section {
+        height: calc(100vh - 200px);
+        background-color: #16161a;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 50px;
+        padding: 50px 0;
+    }
+
+    .bio {
+        max-width: 850px;
+        text-align: center;
+    }
+
+        .projects {
+        display: flex;
+        gap: 20px;
+        justify-content: center;
+    }
+
+    /* carte */
+    .card {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        width: 300px;
+        height: 300px;
+        background: #16161a;
+        border-radius: 12px;
+        overflow: hidden;
+        box-shadow: 10px 10px 20px black;
+
+        transition: transform 0.3s ease, box-shadow 0.3s ease;
+    }
+
+    /* image */
+    .card-image {
+        height: 150px;
+        width: 150px;
+        filter: drop-shadow(10px 10px 10px black);
+    }
+
+    /* contenu */
+    .card-content {
+        padding: 15px;
+        text-align: center;
+    }
+
+    .card>h3 {
+        color: white;
+        font-size: 16px;
+        margin: 10px 0;
+    }
+
+    /* bouton */
+    .card button {
+        padding: 8px 12px;
+        border: none;
+        border-radius: 6px;
+        background: #7f5af0;
+        color: white;
+        cursor: pointer;
+    }
+
+    /* hover effet */
+    .card:hover {
+        transform: translateY(-5px);
+        box-shadow: 10px 10px 20px black;
+    }
+</style>
+
