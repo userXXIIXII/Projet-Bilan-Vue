e49c10afac55c48504136e982370f50ed9768c59
Aarin Deb <aarindeb2003@gmail.com>
2026-05-09 23:38:10 +0200

    Changement de pp et de fond

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <section id="Home">
-        <img src="@/assets/introPic.jpg" class="pp" alt="Photo de Aarin Deb">
+        <img src="@/assets/photoHome.png" class="pp" alt="Photo de Aarin Deb">
         <p>Salut!👋 Je suis</p>
         <h1>Aarin DEB</h1>
         <p>
